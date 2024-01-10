f91a4dca55fe3a0c2d50fcd19b9064456647831e
samuelahmed <samueladuval@gmail.com>
2024-01-09 19:09:01 -0800

    start demo layout

--- src/components/demoComponent.tsx
+++ src/components/demoComponent.tsx
@@ -1,13 +1,28 @@
-
-import { Button } from "./ui/button"
+import { Button } from "./ui/button";
 
 export default function DemoComponent() {
-  
   return (
     <>
-      <Button variant="default">Button</Button>
+    <h1 className="text-xl py-2 px-2">UI component demo</h1>
+      <div className="space-x-1 py-2 px-2">
+        <h4>Buttons</h4>
+        <Button variant="default">default</Button>
+        <Button variant="secondary">secondary</Button>
+        <Button variant="destructive">destructive</Button>
+        <Button variant="outline">outline</Button>
+        <Button variant="ghost">ghost</Button>
+        <Button variant="link">link</Button>
+      </div>
+      <div className="space-x-1 py-2 px-2">
+        <h4>Menubar</h4>
+      </div>
+      <div className="space-x-1 py-2 px-2">
+        <h4>Input</h4>
+      </div>
+      <div className="space-x-1 py-2 px-2">
+        <h4>Image</h4>
+      </div>
 
-      <button>button two</button>
     </>
   );
 }
