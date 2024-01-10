b90c245f4d1426b6ae389acc0008f413023417ca
samuelahmed <samueladuval@gmail.com>
2024-01-09 16:38:57 -0800

    sync up

---
+++ src/components/demoComponent.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function DemoComponent() {
+  return (
+    <>
+      <Button variant="default">Button</Button>
+
+      <button>button two</button>
+    </>
+  );
+}
