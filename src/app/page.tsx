0abfcf62d8c0c8c6044bae695fd043d39e7ed9cc
samuelahmed <samueladuval@gmail.com>
2024-01-09 18:59:33 -0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import DemoComponent from "../components/demoComponent"
-// import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="">
       <DemoComponent />
-      {/* <Button>TEST</Button> */}
     </main>
   );
 }
