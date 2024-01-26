562b7b034bc39f56ce5fa32a4ec3f4bb57c3f512
samuelahmed <samueladuval@gmail.com>
2024-01-26 14:38:16 -0800

    payment page info and outline img schema

--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -1,33 +1,28 @@
-// @ts-nocheck
 "use client";
 
 import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
 
 export default function Payments() {
-
   const onSubscribe = async () => {
     try {
-      const response = await fetch("/api/stripe")
-      const data = await response.json()
+      const response = await fetch("/api/stripe");
+      const data = await response.json();
 
-      window.location.href = data.url
+      window.location.href = data.url;
     } catch (error) {
-      console.log('something went wrong', error)
+      console.log("something went wrong", error);
     }
-  }
+  };
   return (
     <div className="backgroundStyle h-screen w-screen flex flex-col">
       <Header />
       <main className="flex flex-col flex-grow items-center space-y-10 mt-10">
-        <div className="w-4/5 md:w-1/2 flex flex-col space-y-2 ">
-
-          <Button
-          onClick={onSubscribe}
-          >
-            Subscribe
-          </Button>
-          add stripe payment component here
+        <div className="w-4/5 md:w-1/2 flex flex-col space-y-2">
+          {/* add loading state to this button */}
+          <Button onClick={onSubscribe}>Payments Dashboard</Button>
+          <div>Free Images: 7 / 10</div>
+          <div>Monthly Images: 90 / 100</div>
         </div>
       </main>
     </div>
