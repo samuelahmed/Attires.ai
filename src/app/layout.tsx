f841e5e24f76c3368ca6ac6a83cbcd126f454f5a
samuelahmed <samueladuval@gmail.com>
2024-02-03 03:38:32 -0800

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
-let url = "https://www.attires.ai";
-let ogImage = "https://www.attires.ai/logo.png";
-let name = "meow";
+// let url = "https://www.attires.ai";
+// let ogImage = "https://www.attires.ai/logo.png";
+// let name = "meow";
 let description = "Your personal virtual wardrobe";
 
 export const metadata: Metadata = {
@@ -16,39 +16,44 @@ export const metadata: Metadata = {
 
   title: "Attires.ai",
   description: description,
-  keywords: [
-    // "Next.js",
-    // "React",
-    // "Tailwind CSS",
-    // "Server Components",
-    // "Radix UI",
-  ],
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: url,
-    title: name,
-    description: description,
-    siteName: name,
-    images: [
-      {
-        url: ogImage,
-        width: 1200,
-        height: 630,
-        // width: 1200,
-        // height: 630,
-        alt: name,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: name,
-    description: description,
-    images: [ogImage],
-    creator: "Attires.ai",
-  },
-};
+}
+
+
+//   keywords: [
+//     // "Next.js",
+//     // "React",
+//     // "Tailwind CSS",
+//     // "Server Components",
+//     // "Radix UI",
+//   ],
+//   openGraph: {
+//     type: "website",
+//     locale: "en_US",
+//     url: url,
+//     title: name,
+//     description: description,
+//     siteName: name,
+//     images: [
+//       {
+//         url: ogImage,
+//         width: 1200,
+//         height: 630,
+//         // width: 1200,
+//         // height: 630,
+//         alt: name,
+//       },
+//     ],
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: name,
+//     description: description,
+//     images: [ogImage],
+//     creator: "Attires.ai",
+//   },
+// };
+
+
 export default function RootLayout({
   children,
 }: {
