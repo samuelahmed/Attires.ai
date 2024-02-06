eae5ffdaeafe90e69e3a1eb320697aedfa5b55d1
samuelahmed <samueladuval@gmail.com>
2024-02-06 01:16:38 -0800

    add toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,8 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           {children}
+          <Toaster />
+
           <Analytics />
         </body>
       </html>
