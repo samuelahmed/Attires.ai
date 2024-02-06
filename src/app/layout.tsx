df8498acbc9ce201714538ed8573c9fb8660e999
samuelahmed <samueladuval@gmail.com>
2024-02-05 16:03:03 -0800

    delete unused metatdata scheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,52 +6,11 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// let url = "https://www.attires.ai";
-// let ogImage = "https://www.attires.ai/logo.png";
-// let name = "meow";
-let description = "Your personal virtual wardrobe";
-
 export const metadata: Metadata = {
-  metadataBase: new URL('https://www.attires.ai'),
+  metadataBase: new URL("https://www.attires.ai"),
   title: "Attires.ai",
-  description: description,
-}
-
-
-//   keywords: [
-//     // "Next.js",
-//     // "React",
-//     // "Tailwind CSS",
-//     // "Server Components",
-//     // "Radix UI",
-//   ],
-//   openGraph: {
-//     type: "website",
-//     locale: "en_US",
-//     url: url,
-//     title: name,
-//     description: description,
-//     siteName: name,
-//     images: [
-//       {
-//         url: ogImage,
-//         width: 1200,
-//         height: 630,
-//         // width: 1200,
-//         // height: 630,
-//         alt: name,
-//       },
-//     ],
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     title: name,
-//     description: description,
-//     images: [ogImage],
-//     creator: "Attires.ai",
-//   },
-// };
-
+  description: "Your personal virtual wardrobe",
+};
 
 export default function RootLayout({
   children,
