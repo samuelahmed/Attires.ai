1af71e33157e9251d34fad6e6902ec959a727d68
samuelahmed <samueladuval@gmail.com>
2024-02-09 16:25:59 -0800

    more logging & reload with tmp variable

--- src/app/api/images/uploadMaskToS3/pipeline.js
+++ src/app/api/images/uploadMaskToS3/pipeline.js
@@ -8,7 +8,14 @@ const P = () =>
 
     static getInstance(progress_callback = null) {
       if (this.instance === null) {
-        this.instance = pipeline(this.task, this.model, { progress_callback });
+        // this.instance = pipeline(this.task, this.model, { progress_callback });
+        // this.instance = pipeline(this.task, this.model, { progress_callback, cache_dir: '/tmp' });
+        console.log("Before pipeline call");
+        this.instance = pipeline(this.task, this.model, {
+          progress_callback,
+          cache_dir: "/tmp",
+        });
+        console.log("After pipeline call");
       }
       return this.instance;
     }
