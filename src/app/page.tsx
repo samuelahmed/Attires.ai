0beb8de55beef35253148fd96f60177635bb91b8
samuelahmed <samueladuval@gmail.com>
2024-02-27 21:31:33 -0800

    replace vid 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,6 +101,7 @@ export default function Home() {
             </video>
           </div>
           <div>
+            
             <p className="text-center font-semibold">3. Perfect the mask</p>
             <video
               className="shadow-[rgba(6,_24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px_-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset]"
