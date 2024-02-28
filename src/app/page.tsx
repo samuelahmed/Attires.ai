14b5dcefb2774905277f52b5a22333e2e35a75b3
samuelahmed <samueladuval@gmail.com>
2024-02-27 20:45:55 -0800

    test playsinline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,6 +70,8 @@ export default function Home() {
               className="shadow-[rgba(6,_24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px_-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset]"
               style={{ minHeight: "438px" }}
               loop
+              playsInline
+
               ref={videoRef1}
               width="320"
               height="240"
